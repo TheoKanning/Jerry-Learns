adf3efe9d50e8969a0b404e1ce8ea46c21122d38
Theo Kanning <theokanning@gmail.com>
2017-09-11 20:50:47 -0500

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Walking with Neural Networks',
+    version='0.1',
+    description='An experimental simulation of human motion using neural networks',
+    author='Theo Kanning',
+    url='https://github.com/TheoKanning/Jerry-Learns',
+    license='GPL-3.0'
+)
